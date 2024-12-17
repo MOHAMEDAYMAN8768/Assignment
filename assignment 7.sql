4ca0b0f8289e90ee0977fd68fbfac32a1ba64f91
Mohamed Elgahreb <112842567+MOHAMEDAYMAN8768@users.noreply.github.com>
2024-12-17 17:00:11 +0200

    Add files via upload

--- assignment 7.sql
+++ assignment 7.sql
@@ -11,32 +11,66 @@ JOIN
 ON 
     e.department_id = d.department_id;
 
+
+Select * from vw_employee_details;
+
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
 -- Task 2: Modifying a View
-CREATE OR REPLACE VIEW vw_work_hrs AS
-SELECT 
-    e.first_name AS first_name,
-    e.last_name AS last_name,
-    p.pname AS project_name,
-    w.hours AS work_hours
+CREATE VIEW vw_work_hrs AS
+SELECT
+    e.employee_id,
+    e.first_name, 
+    e.last_name, 
+    e.job_id, 
+    j.job_title, 
+    h.start_date, 
+    h.end_date,
+	DATEDIFF(month, h.start_date, h.end_date) AS Work_Experience_years
 FROM 
     employees e
-JOIN 
-    work_hours w 
-ON 
-    e.employee_id = w.employee_id
-JOIN 
-    projects p 
-ON 
-    w.project_id = p.project_id
-WHERE 
-    e.department_id = 5;
+INNER JOIN 
+    jobs j 
+    ON e.job_id = j.job_id
+INNER JOIN 
+    job_history h 
+    ON e.employee_id = h.employee_id;
+GO
 
+
+
+select * from vw_work_hrs;
+
+
+
+---------------------------------------------
+---------------------------------------------
+---------------------------------------------
 -- Task 3: Creating Views with Check Option
-CREATE VIEW vw_high_status_suppliers AS
-SELECT 
-    *
+Drop view higher_salary;
+CREATE VIEW higher_salary AS 
+WITH avg_salary AS (
+    SELECT AVG(salary) AS avg_sal 
+    FROM employees
+)
+SELECT
+    e.employee_id,
+    d.department_name,
+    j.job_title,
+    e.first_name,
+    e.last_name,
+    e.salary
 FROM 
-    suppliers
+    employees e
+INNER JOIN 
+    departments d 
+    ON d.department_id = e.department_id
+INNER JOIN 
+    jobs j 
+    ON e.job_id = j.job_id
 WHERE 
-    status > 15
+    e.salary > (SELECT avg_sal FROM avg_salary)
 WITH CHECK OPTION;
+
+select * from higher_salary order by salary desc;
